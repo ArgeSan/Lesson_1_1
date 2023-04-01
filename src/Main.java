a1a50dace6abd061f329b192d7ac67f7fc9d6554
ArgeSan <129587675+ArgeSan@users.noreply.github.com>
2023-04-01 20:30:44 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String name = "Argen";
+
+        final int NUM = 2008;
+
+        String word = "Madaminov";
+
+        String Argen = word + " tk " + NUM;
+        System.out.println(Argen);
+
+        String allVariables = Argen + word + NUM;
+        System.out.println(allVariables);
+
+        if (NUM < 0) {
+            System.out.println("Вы сохранили отрицательное число");
+        }else  if (NUM > 0) {
+            System.out.println("Вы сохранили положительное число");
+        }else {
+            System.out.println("Вы сохранили ноль");
+        }
+
+
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите ваше имя: ");
+        name = sc.nextLine();
+        System.out.println("Hello " + name + "!");
     }
 }
